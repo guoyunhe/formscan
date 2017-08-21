f924fbe3104e07b123f8db0592387c9f71598c08
Guo Yunhe <guoyunhebrave@gmail.com>
2017-08-21 17:53:43 +0300

    Tesseract OCR

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-var fs      = require('fs');
-var path    = require('path');
+var fs = require('fs');
+var path = require('path');
 var pdf2img = require('pdf2img');
- 
-var input   = path.join(__dirname , 'data', 'input.pdf');
- 
+var tesseract = require('node-tesseract');
+
+var input = path.join(__dirname, 'data', 'input.pdf');
+
 pdf2img.setOptions({
-  type: 'png',                                // png or jpg, default jpg 
-  size: 1024,                                 // default 1024 
-  density: 600,                               // default 600 
-  outputdir: path.join(__dirname, 'data'),  // output folder, default null (if null given, then it will create folder name same as file name) 
-  outputname: 'input',                         // output file name, dafault null (if null given, then it will create image name same as input name) 
-  page: null                                  // convert selected page, default null (if null given, then it will convert all pages) 
+    type: 'png',                                // png or jpg, default jpg 
+    size: 2048,                                 // default 1024 
+    density: 600,                               // default 600 
+    outputdir: path.join(__dirname, 'data'),  // output folder, default null (if null given, then it will create folder name same as file name) 
+    outputname: 'input',                         // output file name, dafault null (if null given, then it will create image name same as input name) 
+    page: null                                  // convert selected page, default null (if null given, then it will convert all pages) 
 });
- 
-pdf2img.convert(input, function(err, info) {
-  if (err) console.log(err)
-  else console.log(info);
+
+pdf2img.convert(input, function (err, info) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    info.message.map(function (image) {
+        tesseract.process(image.path, { l: 'fin' }, function (err, text) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+
+            fs.writeFile(image.path + '.txt', text, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
+        });
+    });
 });
