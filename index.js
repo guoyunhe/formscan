f72ec57beae56abfe39f3d1fd2d88b34c06a8391
Guo Yunhe <guoyunhebrave@gmail.com>
2017-08-24 17:29:22 +0300

    Crop value areas from pages

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var fs = require('fs');
 var path = require('path');
+var gm = require('gm');
 var pdf2img = require('pdf2img');
 var tesseract = require('node-tesseract');
+
+var structure = JSON.parse(fs.readFileSync(path.join(__dirname, 'structure.json'), 'utf8'));
+
+console.log(structure);
 
 var input = path.join(__dirname, 'data', 'input.pdf');
 
@@ -9,8 +14,8 @@ pdf2img.setOptions({
     type: 'png',                                // png or jpg, default jpg 
     size: 2048,                                 // default 1024 
     density: 600,                               // default 600 
-    outputdir: path.join(__dirname, 'data'),  // output folder, default null (if null given, then it will create folder name same as file name) 
-    outputname: 'input',                         // output file name, dafault null (if null given, then it will create image name same as input name) 
+    outputdir: path.join(__dirname, 'data', 'pages'),    // output folder, default null (if null given, then it will create folder name same as file name) 
+    outputname: 'page',                   // output file name, dafault null (if null given, then it will create image name same as input name) 
     page: null                                  // convert selected page, default null (if null given, then it will convert all pages) 
 });
 
@@ -20,7 +25,39 @@ pdf2img.convert(input, function (err, info) {
         return;
     }
 
-    info.message.map(function (image) {
+    info.message.map(function (image, i) {
+        gm(image.path).size(function (err, size) {
+            if (structure[i]) {
+                structure[i].map(function (area, j) {
+                    let outputPath = path.join(__dirname, 'data', 'pages', 'area_' + i + '_' + j + '.png');
+                    gm(image.path).crop(
+                        size.width * (area.boundary.right - area.boundary.left),
+                        size.height * (area.boundary.bottom - area.boundary.top),
+                        size.width * area.boundary.left,
+                        size.height * area.boundary.top
+                    ).write(outputPath, function (err) {
+                        if (err) {
+                            console.log(err);
+                            return;
+                        }
+
+                        if (area.type === 'text') {
+                            tesseract.process(outputPath, { l: 'fin' }, function (err, text) {
+                                if (err) {
+                                    console.error(err);
+                                    return;
+                                }
+
+                                console.log(area.name + ': ' + text);
+                            });
+                        } else if (area.type === 'checkbox') {
+
+                        }
+                    });
+                });
+            }
+        });
+
         tesseract.process(image.path, { l: 'fin' }, function (err, text) {
             if (err) {
                 console.error(err);
